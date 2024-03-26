e203f660719bd3857046291ec479dc26bde47da4
gheo-grandma <bonetto.matteof@gmail.com>
2024-03-26 21:04:43 +0100

    Update decimal_to_binary.py

--- decimal_to_binary.py
+++ decimal_to_binary.py
@@ -1,7 +1,8 @@
 def intToBin(n):
     
-    # base case: the number is either odd or even
-    # this condition checks if the last number is a 0 or a 1
+    # base case: when we reach 2 // 2 = 1 it means we reached the last number of the list
+    # since the largest number usually is included in the series, we need to append a 1 to the left
+    
     if n // 2 == 0 or n // 2 == 1:
         return '1'
         
@@ -12,10 +13,17 @@ def intToBin(n):
 
 
 
-print(intToBin(17))
+print(intToBin(18))
 
 
 
 # HOW IT WORKS
 
-# hhh
+# binary numbers can be calculated by integer division recursively, 
+# and the remainder is the binary value ( 0 / 1 )
+
+# example for n = 17
+# 17 // 2 = 8       17 % 2 = 1      binary = 1
+# 8 // 2 = 4        8 % 2 = 0       binary = 01
+# 4 // 2 = 2        4 % 2 = 0       binary = 001
+# 2 // 2 = 1    ->  end of the function, so append a 1 to the left      binary = 1001
